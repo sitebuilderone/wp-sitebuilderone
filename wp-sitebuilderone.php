18e3a88fce5f1647c51a2466d074214c51b9cd33
SiteBuiderOne <anton@sitebuilderone.com>
2025-01-20 17:45:46 -0800

    updated plugin file format

--- wp-sitebuilderone.php
+++ wp-sitebuilderone.php
@@ -1,5 +1,19 @@
-/*
- Plugin Name: SiteBuilderOne Local Business
- GitHub Plugin URI: https://github.com/sitebuilderone/wp-sitebuilderone
- GitHub Branch: main
-*/
+<?php
+/**
+ * Plugin Name: SiteBuilderOne Local Business
+ * Plugin URI:        https://github.com/sitebuilderone/wp-sitebuilderone
+ * GitHub Plugin URI: https://github.com/sitebuilderone/wp-sitebuilderone
+ * GitHub Branch: main
+ * Description:       ACF, LiveCanvas compatible plugin for Local Business websites
+ * Version:           0.0.2
+ * Author:            sitebuilderone.com
+ * Author URI:        https://github.com/sitebuilderone
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       admin-site-enhancements
+ * 
+ */
+// If this file is called directly, abort.
+if ( !defined( 'ABSPATH' ) ) {
+    exit;
+}
